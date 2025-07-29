f60512dbb96d1c4fcc38f39f5188cfb2186d98d5
Mayara Gonçalves <mayarahelenag@gmail.com>
2025-07-28 21:16:01 -0300

    projeto final

--- mapa.js
+++ mapa.js
@@ -23,8 +23,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     observer.observe(mapaDiv);
   } else {
-    // Se o navegador não suportar IntersectionObserver, carrega direto:
-    mapaDiv.innerHTML = `
+        mapaDiv.innerHTML = `
       <a href="https://www.google.com/maps/place/R.+Chabad,+124+-+Jardim+Paulista,+São+Paulo" target="_blank" rel="noopener noreferrer" style="display: inline-block; border: 2px solid #ccc; border-radius: 8px; overflow: hidden;">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.193389216818!2d-46.6727108!3d-23.5614969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce577f80c0fc55%3A0xebdfd4a56f47cd14!2sR.%20Chabad%2C%20124%20-%20Jardim%20Paulista%2C%20São%20Paulo%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1753676018054!5m2!1spt-BR!2sbr"
